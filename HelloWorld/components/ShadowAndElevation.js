e4d841485bc26c2612d45c7d64eb22c21bb35255
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-03-12 00:58:14 +0100

    covered the basics of react-native

---
+++ HelloWorld/components/ShadowAndElevation.js
@@ -0,0 +1,50 @@
+// import { View, StyleSheet, Text } from "react-native";
+
+// export default function ShadowAndElevation() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={[styles.box, styles.lightblueBg, styles.boxShadow]}>
+//         <Text>Lightblue Box</Text>
+//       </View>
+
+//       <View style={[styles.box, styles.lightgreenBg, styles.androidShadow]}>
+//         <Text>Lightgreen Box</Text>
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "plum",
+//     paddingHorizontal: 10,
+//     paddingVertical: 20,
+//     marginVertical: 10,
+//     borderWidth: 2,
+//     borderColor: "purple",
+//   },
+//   box: {
+//     width: 250,
+//     height: 250,
+//     padding: 10,
+//   },
+//   lightblueBg: {
+//     backgroundColor: "lightblue",
+//   },
+//   lightgreenBg: {
+//     backgroundColor: "lightgreen",
+//   },
+//   boxShadow: {
+//     shadowColor: "#333333",
+//     shadowOffset: {
+//       height: 6,
+//       width: 6,
+//     },
+//     shadowOpacity: 0.6,
+//     shadowRadius: 4,
+//   },
+//   androidShadow: {
+//     elevation: 10,
+//   },
+// });
