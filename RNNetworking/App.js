cb5f9de21a13a316056a471c5a81a0c3c1a039e6
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-03-12 01:45:05 +0100

    covered networking in react native

---
+++ RNNetworking/App.js
@@ -0,0 +1,204 @@
+import { useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  StatusBar,
+  View,
+  StyleSheet,
+  FlatList,
+  Text,
+  ActivityIndicator,
+  TextInput,
+  Button,
+} from "react-native";
+
+export default function App() {
+  const [postList, setPostList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const [isPosting, setIsPosting] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchData = async (limit = 10) => {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+      );
+      const data = await response.json();
+      setPostList(data);
+      setIsLoading(false);
+      setError("");
+    } catch (error) {
+      console.log("Error fetching data:", error);
+      setIsLoading(false);
+      setError("Failed to fetch post list");
+    }
+  };
+
+  const handleRefresh = () => {
+    setRefreshing(true);
+    fetchData(20);
+    setRefreshing(false);
+  };
+
+  const addPost = async () => {
+    setIsPosting(true);
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: postTitle,
+            body: postBody,
+          }),
+        }
+      );
+
+      const newPost = await response.json();
+      setPostList([newPost, ...postList]);
+
+      setPostTitle("");
+      setPostBody("");
+      setIsPosting(false);
+      setError("");
+    } catch (error) {
+      console.error("Error adding new post:", error);
+      setError("Failed to add new post");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <SafeAreaView style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="0000ff" />
+        <Text>Loading ...</Text>
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {error ? (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{error}</Text>
+        </View>
+      ) : (
+        <>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Post title"
+              value={postTitle}
+              onChangeText={setPostTitle}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Post body"
+              value={postBody}
+              onChangeText={setPostBody}
+            />
+            <Button
+              title={isPosting ? "Adding..." : "Add Post"}
+              onPress={addPost}
+              disabled={isPosting}
+            />
+          </View>
+
+          <View style={styles.listContainer}>
+            <FlatList
+              data={postList}
+              renderItem={(item) => {
+                return (
+                  <View style={styles.card} key={item.index}>
+                    <Text style={styles.titleText}>{item.item?.title}</Text>
+                    <Text style={styles.bodyText}>{item.item?.body}</Text>
+                  </View>
+                );
+              }}
+              ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+              ListEmptyComponent={<Text>No Posts Found</Text>}
+              ListHeaderComponent={
+                <Text style={styles.headerText}>Post List</Text>
+              }
+              ListFooterComponent={
+                <Text style={styles.footerText}>End of list</Text>
+              }
+              refreshing={refreshing}
+              onRefresh={handleRefresh}
+            />
+          </View>
+        </>
+      )}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    paddingTop: StatusBar.currentHeight,
+  },
+  listContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  titleText: {
+    fontSize: 30,
+  },
+  bodyText: {
+    fontSize: 24,
+    color: "#666666",
+  },
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: "F5F5F5",
+    paddingTop: StatusBar.currentHeight,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+  },
+  input: {
+    height: 48,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 8,
+    padding: 8,
+    borderRadius: 8,
+  },
+  errorContainer: {
+    backgroundColor: "#FFC0CB",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+    alignItems: "center",
+  },
+  errorText: {
+    color: "#D8000C",
+    fontSize: 16,
+    textAlign: "center",
+  },
+});
